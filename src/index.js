66b554bcb073444c7d7cc545281f967e6fe86ea8
matthew-wright9630 <matthew.wright9630@gmail.com>
2024-06-15 12:07:32 -0400

    update design of website

--- src/index.js
+++ src/index.js
@@ -1,19 +1,34 @@
 import "./pages/index.css";
+import {
+  places,
+  destinationList,
+  destinationSelect,
+} from "./utils/constants.js";
 import { Section } from "./components/Section.js";
 
 const destinationAddBtn = document.querySelector(".destination__add-button");
 
 destinationAddBtn.addEventListener("click", () => {
   console.log("clicked!");
+  console.log(destinationSelect.value);
+  addDestination(destinationSelect.value);
 });
 
 const destinationSection = new Section(
   {
     items: places,
-    renderer: (item, method = "prepend") => {
-      const cardElement = createCard(item);
-      photoCardList[method](cardElement);
+    renderer: (item, method = "append") => {
+      const option = item.name;
+      console.log("test");
+      destinationList[method](option);
     },
   },
-  "#"
+  "#destination-add-form"
 );
+destinationSection.renderItems();
+
+function addDestination(item) {
+    console.log(item);
+  destinationSection.addItem({name: item});
+  console.log(destinationList);
+}
