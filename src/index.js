309c7a2549be45d0518cea9ca3d0cfad7b31ed72
Simon <sawsimonelinn@gmail.com>
2024-06-16 03:13:31 -0700

    add map api

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
-import "./pages/index.css";
-import {
-  places,
-  destinationList,
-  destinationSelect,
-} from "./utils/constants.js";
-import { Section } from "./components/Section.js";
+import './pages/index.css';
+// import { places } from './utils/constants.js';
+// import {
+//   routeSubmitBtn,
+//   places,
+//   destinationList,
+//   destinationSelect,
+// } from './utils/constants.js';
+// import { Section } from './components/Section.js';
 
+// routeSubmitBtn.addEventListener('click', () => {
+//   const checkedRadio = document.querySelector(
+//     'input[name=route-option]:checked'
+//   ).value;
+// });
+
+// console.log(places[0]);
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,9 @@
-const places = [];
 // places.push({name: "San Jose", latitude: 100, longitude: 30}, {name: "Sacramento", latitude: 200, longitude: 30},);
 
-const destinationList = document.querySelector(".destinations__list");
+const places = [];
 
-const destinationSelect = document.querySelector(".destinations-form__list");
+const destinationList = document.querySelector('.destinations__list');
+
+const destinationSelect = document.querySelector('.destinations-form__list');
 
 export { places, destinationList, destinationSelect };
